1abe1f3c0707884088e63dbfda3c66ead5017397
robsonnsbr <robsonnsbr@gmail.com>
2024-12-12 21:05:55 -0300

    layout: add template

--- frontend/src/components/common/container/LayoutWrapper.tsx
+++ frontend/src/components/common/container/LayoutWrapper.tsx
@@ -8,7 +8,7 @@ const LayoutWrapper = ({ children, ...rest }: ILayoutWrapperProps) => {
   return (
     <div
       {...rest}
-      className="relative bg-blue-700 w-full max-w-screen-2xl overflow-hidden"
+      className="relative bg-dark w-full max-w-screen-2xl overflow-hidden"
     >
       {children}
     </div>
