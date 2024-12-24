1fb94f30410afe7938aea2845a47e6ad644fdec9
robsonnsbr <robsonnsbr@gmail.com>
2024-12-23 21:14:22 -0300

    layout: fix color wpp

--- frontend/src/components/common/button/WppButton.tsx
+++ frontend/src/components/common/button/WppButton.tsx
@@ -5,9 +5,9 @@ const WppButton = () => {
   return (
     <button
       id="whats-app-btn"
-      className="fixed bottom-6 right-6 z-50 flex items-center justify-center w-14 h-14 rounded-full bg-green-500 hover:bg-green-600 text-white shadow-lg transition-all duration-300"
+      className="fixed bottom-6 right-6 z-50 flex items-center justify-center w-14 h-14 rounded-full bg-green hover:bg-green-600 text-white shadow-lg transition-all duration-300"
     >
-      <FaWhatsapp size={28} />
+      <FaWhatsapp size={28} className="text-blue" />
     </button>
   );
 };
