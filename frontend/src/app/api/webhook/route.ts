d3072401089c554a72144115f0eb250367f2fe2f
robsonnsbr <robsonnsbr@gmail.com>
2024-12-30 13:58:39 -0300

    teste: ...

--- frontend/src/app/api/webhook/route.ts
+++ frontend/src/app/api/webhook/route.ts
@@ -50,3 +50,10 @@ export async function POST(req: NextRequest) {
     );
   }
 }
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function GET(req: NextRequest) {
+  return NextResponse.json({
+    message: "Olá, esta é uma resposta simples do GET!",
+  });
+}
