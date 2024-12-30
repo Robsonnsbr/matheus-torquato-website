a57789b44427749745adbf9d156b932428066c2e
robsonnsbr <robsonnsbr@gmail.com>
2024-12-29 23:24:29 -0300

    docs: remove commit

--- frontend/src/components/home/components/form-block/TextArea.tsx
+++ frontend/src/components/home/components/form-block/TextArea.tsx
@@ -7,7 +7,7 @@ const TextArea = ({
     <textarea
       required
       className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      {...props} // espalha as props para o elemento textarea
+      {...props}
     />
   );
 };
